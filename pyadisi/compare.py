87df85a162587338aa9b7259b8bcfa20d04d9bda
Isaac Yeaton <isaac.yeaton@gmail.com>
2014-05-27 10:58:55 -0400

    Added file and example notebook on comparing images.

---
+++ pyadisi/compare.py
@@ -0,0 +1,8 @@
+"""Compare to image sequences to get the motion of the animal.
+"""
+
+from __future__ import division
+from __future__ import print_function
+#from __future__ import unicode_literals
+
+import numpy as np
