df2dc5241aeaa9b57da57ee4e37ccbb4564d3033
Isaac Yeaton <isaac.yeaton@gmail.com>
2014-06-01 19:58:21 -0400

    Converted tabs to spaces and starting work on photron function.

--- pyadisi/metadata.py
+++ pyadisi/metadata.py
@@ -3,42 +3,79 @@ from __future__ import division
 import numpy as np
 
 def _s2n(string):
-	"""Convert a string to a float or int.
-	"""
+    """Convert a string to a float or int.
+    """
 
-	return float(string) if '.' in string else int(string)
-	
+    return float(string) if '.' in string else int(string)
+
 
 def fastec(fname):
-	"""Load in metadata for Fastec cameras.
+    """Load in metadata for Fastec cameras.
 
-	Parameters
-	----------
-	fname : str
-	    File to open.
+    Parameters
+    ----------
+    fname : str
+        File to open.
 
-	Returns
-	-------
-	config : dict
-	    Nested dictionary.
-	"""
+    Returns
+    -------
+    config : dict
+        Nested dictionary.
+    """
 
-	config = {}
-	with open(fname, 'r') as fp:
-		lines = fp.readlines()
-		
-	for idx, line in enumerate(lines):
-		if line.startswith('['):
-			print line
-			param = line[1:-2]
-			d = {}
-		if line.startswith('\t'):
-			key, val = line[1:-1].split('=')
-			if val.isdigit():  # this will be a number
-				val = _s2n(val)
-			elif val.startswith('['):  # this will be a list
-			    val = map(_s2n, val[1:-1].split(','))
-			d[key] = val
-		if line.startswith('\n') or idx == len(lines) - 1:
-			config[param] = d
-	return config
+    with open(fname, 'r') as fp:
+        lines = fp.readlines()
+
+    config = {}
+    for idx, line in enumerate(lines):
+        if line.startswith('['):
+            print line
+            param = line[1:-2]
+            d = {}
+        if line.startswith('\t'):
+            key, val = line[1:-1].split('=')
+            if val.isdigit():  # this will be a number
+                val = _s2n(val)
+            elif val.startswith('['):  # this will be a list
+                val = map(_s2n, val[1:-1].split(','))
+            d[key] = val
+        if line.startswith('\n') or idx == len(lines) - 1:
+            config[param] = d
+
+    return config
+
+
+def photron(fname):
+    """Load in metadata for Photron cameras.
+
+    Parameters
+    ----------
+    fname : str
+        File to open.
+
+    Returns
+    -------
+    config : dict
+        Nested dictionary.
+    """
+
+    with open(fname, 'r') as fp:
+        lines = fp.readlines()
+
+    config = {}
+    for idx, line in enumerate(lines):
+        if line.startswith('['):
+            print line
+            param = line[1:-2]
+            d = {}
+        if line.startswith('\t'):
+            key, val = line[1:-1].split('=')
+            if val.isdigit():  # this will be a number
+                val = _s2n(val)
+            elif val.startswith('['):  # this will be a list
+                val = map(_s2n, val[1:-1].split(','))
+            d[key] = val
+        if line.startswith('\n') or idx == len(lines) - 1:
+            config[param] = d
+
+    return lines
