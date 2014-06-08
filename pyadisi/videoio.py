d0df352a167acb7e6ccff9fcd248845613670041
Isaac Yeaton <isaac.yeaton@gmail.com>
2014-06-08 01:08:33 -0400

    Added licence and attribution to videoio.py.

--- pyadisi/videoio.py
+++ pyadisi/videoio.py
@@ -1,3 +1,84 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Zulko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# The MIT License (MIT)
+# [OSI Approved License]
+#
+# https://github.com/Zulko/moviepy
+#
+# source files:
+# moviepy/conf.py
+# moviepy/video/io/ffmpeg_reader.py
+# moviepy/tools.py
+#
+# Files heavily edited by PIMS contributors
+# January 2014
+
+## Name (and locatio if needed) of the FFMPEG binary. It will be
+## "ffmpeg" on linux, certainly "ffmpeg.exe" on windows, else any path.
+## If not provided (None), the system will look for the right version
+## automatically each time you launch moviepy.
+## If you run this script file it will check that the
+## path to the ffmpeg binary (FFMPEG_BINARY)
+
+# Copyright Notice and Statement for the PIMS Project
+# ===================================================
+#
+#    Copyright (c) 2013-2014 PIMS contributors
+#    https://github.com/soft-matter/pims
+#    All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the soft-matter organization nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# This file contains highly modified code from the PIMS project
+# at https://github.com/soft-matter/pims.
+#
+# source files: pims/base_frames.py
+#
+# The numpy header writing code and how to deal with .npy files was
+# modified from:
+# github.com/numpy/numpy/blob/master/numpy/lib/format.py
+# github.com/numpy/numpy/blob/master/numpy/lib/npyio.py
+
 from __future__ import division
 
 import numpy as np
